3d174d253b2274069a052ba70a5741a09cd95e47
songtingxu-okta <songting.xu@okta.com>
2019-04-09 15:37:16 -0700

    Create create_downstream_monolith_commit.sh

---
+++ scripts/create_downstream_monolith_commit.sh
@@ -0,0 +1,3 @@
+cd ${OKTA_HOME}/okta-core/clients/loginpage
+echo "Update okta-signin-widget version to ${upstream_artifact_version}"
+yarn upgrade @okta/okta-signin-widget ${upstream_artifact_version}
